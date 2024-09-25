70178e3291a66f8fcd50510cdcd93ae15cf18c3f
UditJain2622004 <uditjain2622004@gmail.com>
2024-09-25 11:52:22 +0530

    update home page

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Button } from '../button'
+import React from "react";
+import { Button } from "../button";
 
 function Header() {
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-      <img src='/logo.svg'/>
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img src="/logo6.png" alt="" />
       <div>
         <Button>Sign In</Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Button } from '../button'
+import React from "react";
+import { Button } from "../button";
 function Hero() {
   return (
-    <div className='flex flex-col items-center mx-56 gap-9'>
-      <h1 className='font-extrabold text-[60px] text-center mt-16'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      <span className='text-[#f56551]'> Discover your next adventure with AI</span></h1>
-      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel companion</p>
-        <Button>Get Started, It's free</Button>
-    
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[60px] text-center mt-16">
+        <span className="font-sans tracking-wide text-gray-900">
+          TRIP-E-DUNIYA
+        </span>
+        <br></br>
+        <span className="text-[#f56551]">
+          {" "}
+          Discover your next adventure with AI
+        </span>
+      </h1>
+      <p className="text-xl text-gray-500 text-center">
+        Your personal trip planner and travel companion
+      </p>
+      <Button>Get Started, It's free</Button>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
