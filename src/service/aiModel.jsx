c7bb041c9d9838b94d0db1a979302da9c21e2445
UditJain2622004 <uditjain2622004@gmail.com>
2024-11-01 20:59:23 +0530

    final commit (hopefully)

--- src/service/aiModel.jsx
+++ src/service/aiModel.jsx
@@ -59,5 +59,5 @@ export const chatSession = model.startChat({
   ],
 });
 
-const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-console.log(result.response.text());
+// const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+// console.log(result.response.text());
